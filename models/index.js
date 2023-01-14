ff7bb7cb9ddf0ca571f07eb8255adf193ac0d7f9
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-14 01:17:28 -0800

    route files changed

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//
+// For Thought and User Models
+const Thought = require('./Thought');
+const User = require('./User');
+//
+module.exports = {Thought, User};
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+// 
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Initiate app and Port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Use routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+// Mongoose DB connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Logs mono queries
+mongoose.set('debug', true);
+// Connects and listens on port 3001
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
